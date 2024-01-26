4d331001bdc2c5da08c82ec11e69bd26398fdda3
Okogun Melody <okogunmelody@gmail.com>
2024-01-26 17:35:22 +0100

    created an extended class called QuoteCard

--- lib/main.dart
+++ lib/main.dart
@@ -56,44 +56,56 @@ class QuoteList extends StatefulWidget {
   ];
 
 Widget quoteTemplate(quote){
-  return Card(
-    margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-    child: Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Text(
-            quote.text,
-            style: TextStyle(
-              fontSize: 18.0,
-              color: Colors.grey[600],
-            ),
-            ),
-            SizedBox(height: 6.0,),
-      
-            Text(
-            quote.author,
-            style: TextStyle(
-              fontSize: 14.0,
-              color: Colors.grey[800],
-            ),
-            ),
-
-            SizedBox(height: 8.0),
-            ElevatedButton.icon(
-              onPressed:(){},
-              label: Text('delete quote'),
-              icon: Icon(Icons.delete),
-            )
-        ]
-        ),
-    ),
-  );
+  return QuoteCard(quote:quote);
 }
 
   @override
   _QuoteListState createState() => _QuoteListState();
+}
+
+class QuoteCard extends StatelessWidget {
+  
+  final Quote quote;
+  QuoteCard({required this.quote});
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+              ),
+              SizedBox(height: 6.0,),
+        
+              Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+              ),
+    
+              // SizedBox(height: 8.0),
+              // ElevatedButton.icon(
+              //   onPressed:(){},
+              //   label: Text('delete quote'),
+              //   icon: Icon(Icons.delete),
+             // )
+          ]
+          ),
+      ),
+    );
+  }
 }
 
 class _QuoteListState extends State<QuoteList> {
