559bf2dc8a13cc4b0b2e11853f02ff6d801ae5ec
Okogun Melody <okogunmelody@gmail.com>
2024-01-26 17:53:13 +0100

    created a seperate quote_card file

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@
  //CARD FORMAT  
 import 'package:flutter/material.dart';
 import 'quote.dart';
-
+import 'quote_card.dart';
 void main()=> runApp(MaterialApp(
   home: QuoteList(),
 )
@@ -77,47 +77,3 @@ class _QuoteListState extends State<QuoteList> {
 }
  
 
-class QuoteCard extends StatelessWidget {
-  
-  final Quote quote;
-  QuoteCard({required this.quote});
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600],
-              ),
-              ),
-              SizedBox(height: 6.0,),
-        
-              Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800],
-              ),
-              ),
-    
-              // SizedBox(height: 8.0),
-              // ElevatedButton.icon(
-              //   onPressed:(){},
-              //   label: Text('delete quote'),
-              //   icon: Icon(Icons.delete),
-             // )
-          ]
-          ),
-      ),
-    );
-  }
-}
